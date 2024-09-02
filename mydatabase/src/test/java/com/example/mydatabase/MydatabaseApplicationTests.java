124518b7e1f5d5f1c7238e86e33a1251361ddfd3
abdoulfataho <yamyaogofataho@gmail.com>
2024-09-02 10:48:10 -0400

    final

---
+++ mydatabase/src/test/java/com/example/mydatabase/MydatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
